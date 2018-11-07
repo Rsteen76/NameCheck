e594144a5e201cb7801e1b9397f48339d80cf533
Ryan Steen <ryan@ryansteen.com>
2018-11-06 22:54:01 -0800

    Changes baseURL

--- src/config/http.js
+++ src/config/http.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const http = axios.create({
   // baseURL: 'https://brandorium.herokuapp.com/api'
-  baseURL: 'http://localhost:8080/api',
+  baseURL: process.env.baseURL
 })
