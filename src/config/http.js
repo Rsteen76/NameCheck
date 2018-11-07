65840e0e70fe831c37e6d165f0c46465d6f49309
Ryan Steen <ryan@ryansteen.com>
2018-11-06 23:05:17 -0800

    New build with baseURL change

--- src/config/http.js
+++ src/config/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  // baseURL: 'https://brandorium.herokuapp.com/api'
-  baseURL: process.env.baseURL
+  baseURL: 'https://brandorium.herokuapp.com/api'
+  // baseURL: process.env.baseURL
 })
